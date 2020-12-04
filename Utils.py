0f095eeaca9bd211961f7f6a8f236ce303c7175a
Raphaël <raphael.pilinski@protonmail.com>
2020-12-04 14:07:49 +0100

    Some updates
    Add discord Intents
    Add help command
    Edit Utils

--- Utils.py
+++ Utils.py
@@ -1,30 +1,35 @@
+from urllib import request
 import discord
-import urllib, json
+import json
 
 
-def get_response(key):
+def get_response(key: str):
     url = get_property_in_json_file("config/config.json", "pi_hole_web_address")
-    open_url = urllib.request.urlopen(url + "admin/api.php" if url[-1] == "/" else url + "/admin/api.php")
+    open_url = request.urlopen(url + "admin/api.php" if url[-1] == "/" else url + "/admin/api.php")
+    json_data = ""
+
     if open_url.getcode() == 200:
         data = open_url.read()
         json_data = json.loads(data)
+
     else:
-        print("UNE FUCKING ERREUR EST SURVENUE !", open_url.getcode())
-    return json_data[key]
+        print("Error : ", open_url.getcode())
+    return None if json_data is None else json_data[key]
 
 
-def get_property_in_json_file(file, value):
+def get_property_in_json_file(file: str, value: str):
     with open(file, 'r') as f:
         info1 = json.load(f)
     return info1[str(value)]
 
 
-def write_property_in_json_file(file, data_categorie, data_for_write):
+def write_property_in_json_file(file: str, data_category: str, data_for_write: str):
     with open(file, 'r') as f:
         value = json.load(f)
-    value[str(data_categorie)] = str(data_for_write)
+    value[str(data_category)] = str(data_for_write)
     with open(file, 'w') as f:
         json.dump(value, f, indent=4)
 
-def embedColor():
+
+def embed_color():
     return discord.Colour.from_rgb(255, 223, 0)
