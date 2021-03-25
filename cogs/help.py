7ffde74b683abf79b40f262fee8d2538322c871a
Raphael Pilinski <raphael.pilinski@protonmail.com>
2021-03-25 14:23:56 +0100

    Cleaning.

---
+++ cogs/help.py
@@ -0,0 +1,25 @@
+from discord.ext import commands
+from utils import utils
+import discord
+import time
+
+
+def setup(bot):
+    bot.add_cog(Settings(bot))
+
+
+class Settings(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def help(self, ctx):
+        prefix = utils.get_property('prefix')
+        embed = discord.Embed(color=utils.embed_color())
+        embed.set_author(name='Commandes de DiscordBotPihole :')
+        embed.add_field(name='Prefix', value=f'`{prefix}set prefix <prefix>`', inline=False)
+        embed.add_field(name='PiHole url :', value=f'`{prefix}set url <PiHole url>`', inline=False)
+        embed.add_field(name='Channel :', value=f'`{prefix}set channel <channel>`', inline=False)
+        embed.set_footer(icon_url=ctx.guild.icon_url,
+                         text=f"Demandé par {ctx.author.display_name} • Le {time.strftime('%d/%m/%Y à %H:%M:%S', time.localtime())}")
+        await ctx.send(embed=embed)
---
+++ utils/utils.py
@@ -0,0 +1,32 @@
+import discord
+import aiohttp
+import json
+
+
+def get_response(key: str):
+    url = get_property('pi_hole_web_address')
+
+    async with aiohttp.ClientSession() as s:
+        async with s.get(url + 'admin/api.php' if url[-1] == '/' else url + '/admin/api.php') as r:
+            if r.status == 200:
+                content = await r.json()
+
+    return None if content is None else content[key]
+
+
+def get_property(value: str):
+    with open('utils/config.json', 'r') as f:
+        info1 = json.load(f)
+    return info1[str(value)]
+
+
+def write_property(data_category: str, data_to_write: str):
+    with open('utils/config.json', 'r') as f:
+        value = json.load(f)
+    value[str(data_category)] = str(data_to_write)
+    with open('utils/config.json', 'w') as f:
+        json.dump(value, f, indent=4)
+
+
+def embed_color():
+    return discord.Colour.from_rgb(255, 223, 0)
